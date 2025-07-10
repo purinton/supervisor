84a52a9aa1ea3fff010e09f5c13cde2d851ae415
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-07-10 13:17:55 -0400

    Move and update custom styles to src/style.css
    Custom CSS previously in template/style.css has been moved to src/style.css and imported in main.mjs. This change centralizes styles within the src directory and updates the import path for consistency.

--- src/main.mjs
+++ src/main.mjs
@@ -1,5 +1,7 @@
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '@mdi/font/css/materialdesignicons.min.css';
+import './style.css';
 
 console.log('Hello from main.mjs!');
